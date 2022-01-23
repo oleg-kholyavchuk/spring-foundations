27b5c385d0e359f4568c2563bb08bf391242ce5d
oleg-kholyavchuk <Oleg.Kholyavchuk@bk.ru>
2022-01-23 20:15:57 +0300

    Spring 23.01.22

---
+++ core/src/main/java/ru/itsjava/Main.java
@@ -0,0 +1,18 @@
+package ru.itsjava;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Animal piglet = new Piglet();
+        ApplicationContext context
+                = new ClassPathXmlApplicationContext("context.xml");
+        Animal piglet = context.getBean("pig", Animal.class);
+
+
+        piglet.say();
+    }
+}
